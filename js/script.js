0438d82fa9acdb928f3f9d9a1c9440215969b81d
Alessio Pancia <alessiop2801@gmail.com>
2020-12-16 15:16:54 +0100

    sostituzione app in this a variabile e rimozione v-if

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ var app = new Vue({
     emails: []
   },
   mounted: function () {
+    var questo = this;
     for (let i = 0; i < 10; i++) {
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then(function (response) {
-        app.emails.push(response.data.response);
+        questo.emails.push(response.data.response);
       });
     };
   }
